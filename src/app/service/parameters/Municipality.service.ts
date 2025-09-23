e043ed9ff1396226ab9e6346825ede50037b1060
JesusCarvajal017 <jesusfernandocarvajal2018@gmail.com>
2025-09-23 07:08:39 -0500

    merge juan guerrero

--- src/app/service/parameters/Municipality.service.ts
+++ src/app/service/parameters/Municipality.service.ts
@@ -3,22 +3,24 @@ import { inject, Injectable } from '@angular/core';
 import { GenericService } from '../api/generic.service';
 import { ModelLogicalDelete } from '../../global/model/logicalDelete.model';
 import { Observable } from 'rxjs';
-import { CreateMunicipality, Municipality } from '../../models/parameters/Municipality.model';
+import { CreateModelMunicipality, Municipality } from '../../models/parameters/Municipality.model';
 
 
 @Injectable({
   providedIn: 'root'
 })
 
-export class MunicipalityService extends GenericService<Municipality, CreateMunicipality, ModelLogicalDelete> {
+export class MunicipalityService extends GenericService<Municipality, CreateModelMunicipality, ModelLogicalDelete> {
   constructor() { 
     super('Municipality')
   }
 
 
-  public MunicipiosDepart(id: any): Observable<Municipality[]> {
-      return this.http.get<Municipality[]>(`${this.urlBase}/list/${id}`);
+  override obtenerTodos(status: number = 1): Observable<Municipality[]> {
+       return this.http.get<Municipality[]>(`${this.urlBase}?status=${status}`);
   }
 
 
 }
+export type { Municipality };
+
