2aa4671404a9de174c2cd8d1d121188effaaefe0
Santiago1481 <ssprosantiago@gmail.com>
2025-09-25 11:22:19 -0500

    Ajutes de diseño en ajustes

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -1,5 +1,5 @@
 // environments/environment.ts
 export const environment = {
-  uri: '192.168.20.29',
-  apiUrl: 'http://192.168.20.29:5052/api'
+  uri: '10.3.234.106',
+  apiUrl: 'http://10.3.234.106:5052/api'
 };
