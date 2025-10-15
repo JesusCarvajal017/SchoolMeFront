87b266778c6bd44ca64d8a289c25d7e6f3cc32dc
Juan Guerrero <guerrerojuanfe2004@gmail.com>
2025-10-15 17:26:01 -0500

    estructura

--- src/app/page/forms/form-document-type/form-document-type.component.ts
+++ src/app/page/forms/form-document-type/form-document-type.component.ts
@@ -12,7 +12,7 @@ import {TuiInputModule, TuiTextfieldControllerModule} from '@taiga-ui/legacy';
 
 import { TuiCheckbox } from '@taiga-ui/kit';
 import { MatIconModule } from "@angular/material/icon";
-import { CreateModelDocumentType, DocumentsType } from '../../../models/parameters/DocumentType.model';
+import { CreateModelDocumentType, DocumentType } from '../../../models/parameters/DocumentType.model';
 
 @Component({
   selector: 'app-form-document-type',
@@ -41,7 +41,7 @@ export class FormDocumentTypeComponent implements OnInit, OnChanges {
   actionDescriptio !: string;
 
   @Input()
-  model?: DocumentsType;
+  model?: DocumentType;
 
   @Output()
   posteoForm = new EventEmitter<CreateModelDocumentType>();
--- src/app/service/parameters/documentType.service.ts
+++ src/app/service/parameters/documentType.service.ts
@@ -2,7 +2,7 @@ import { HttpClient } from '@angular/common/http';
 import { inject, Injectable } from '@angular/core';
 
 
-import { CreateModelDocumentType, DocumentsType } from '../../models/parameters/DocumentType.model';
+import { CreateModelDocumentType, DocumentType } from '../../models/parameters/DocumentType.model';
 import { GenericService } from '../api/generic.service';
 import { ModelLogicalDelete } from '../../global/model/logicalDelete.model';
 import { Observable } from 'rxjs';
@@ -11,12 +11,12 @@ import { Observable } from 'rxjs';
   providedIn: 'root'
 })
 
-export class DocumentTypeService extends GenericService<DocumentsType, CreateModelDocumentType, ModelLogicalDelete> {
+export class DocumentTypeService extends GenericService<DocumentType, CreateModelDocumentType, ModelLogicalDelete> {
   constructor() { 
     super('DocumentType')
   }
 
-  override obtenerTodos(status: number = 1): Observable<DocumentsType[]> {
-    return this.http.get<DocumentsType[]>(`${this.urlBase}?status=${status}`);
+  override obtenerTodos(status: number = 1): Observable<DocumentType[]> {
+    return this.http.get<DocumentType[]>(`${this.urlBase}?status=${status}`);
   }
 }
